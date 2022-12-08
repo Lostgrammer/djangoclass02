5392da1f0d5d32bd64850a626462d6de596d91ef
lostgrammer <carlos_cmvp@hotmail.com>
2022-12-08 11:16:35 -0500

    insertando modelos en blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+#creando un abstract
+class Producto(models.Model):
+    nombre = models.CharField(max_length=50)
+    precio = models.IntegerField
+    peso = models.CharField(max_length=50)
+
+    class Meta:
+        abstract = True
+
+#herencia de clase
+class Refri(Producto):
+    min_temperatura = models.CharField(max_length=50)
+    smart_screen = models.CharField(max_length=50)
+    volumen = models.CharField(max_length=50)
+    marca = models.CharField(max_length=50)
+
+class laptop(Producto):
+    modelo_cpu = models.CharField(max_length=50)
